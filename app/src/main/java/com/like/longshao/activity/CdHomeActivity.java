e35296daeffd1123179b631be2b8aab01c27a4e4
longshao <longshao@longshao-PC>
2017-03-02 18:40:16 +0800

    Initial commit

---
+++ app/src/main/java/com/like/longshao/activity/CdHomeActivity.java
@@ -0,0 +1,15 @@
+package com.like.longshao.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.like.longshao.R;
+
+public class CdHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cd_home);
+    }
+}
